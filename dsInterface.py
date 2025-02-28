06d31755297657aaef658719b4d605dcae4f4315
Vicky9397 <vigneshwarana2712@gmail.com>
2025-02-28 17:28:11 +0530

    some changes in generating response function

--- dsInterface.py
+++ dsInterface.py
@@ -3,7 +3,8 @@ import ollama
 def generate_response(prompt):
     # Using Ollama's streaming response
     for chunk in ollama.chat(model="deepseek-r1:8b", messages=[{"role": "user", "content": prompt}], stream=True):
-        #print (f"{chunk['message']['content']}")  # SSE format
-        yield f"data: {chunk['message']['content']}\n\n"
+        print (chunk['message']['content'], end='', flush=True)  # SSE format
+        # yield f"data: {chunk['message']['content']}\n\n"
+        #yield (chunk['message']['content'], end='', flush=True)
 
-#generate_response("Explain quantum physics in simple terms")
+generate_response("Explain AI in simple terms.")
